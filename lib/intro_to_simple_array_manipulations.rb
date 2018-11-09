e43f6cd8bdae6ea410fffeea8cc649ffd481c9c5
Albina Zakirova <zak1804@gmail.com>
2018-11-09 15:55:36 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ def using_pop(great_hits_of_the_nineties)
 end
 
 def pop_with_arg(chars_in_game_of_thrones)
-  chars_arya_killed =chars_in_game_of_thrones.pop(2)
+  @chars_arya_killed =chars_in_game_of_thrones.pop(2)
 end
 
 def using_shift(my_favorite_cities)
